12e7e04aabcb1d7932790b06cd725c74f6708a7c
stevendee37 <stevenwchang01@gmail.com>
2024-12-02 22:38:58 -0800

    Updated transaction output

--- src/transactions.py
+++ src/transactions.py
@@ -31,9 +31,9 @@ class Post(Transaction):
         match self.hop_number:
             case 0:
                 self.hop_number += 1
-                return [self.post_id, self.username, self.content], 0
+                return {"post_id":self.post_id, "user_id":self.username, "content":self.content}, 0
             case 1:
-                return [self.username], 1
+                return {"user_id":self.username}, 1
 
 class Comment(Transaction):
     @override
@@ -54,9 +54,9 @@ class Comment(Transaction):
         match self.hop_number:
             case 0:
                 self.hop_number += 1
-                return [self.comment_id, self.username, self.comment], 0
+                return {"comment_id":self.comment_id, "user_id":self.username, "comment":self.comment}, 0
             case 1:
-                return [self.username], 1
+                return {"user_id": self.username}, 1
             
 class Follow(Transaction):
     @override
@@ -78,12 +78,12 @@ class Follow(Transaction):
             case 0:
                 self.hop_number += 1
                 # Return unique edgeID to identify if edge already exists in table
-                return [f"{self.username}-{self.follower}"], 0
+                return {"edge_id":f"{self.username}-{self.follower}"}, 0
             case 1:
                 self.hop_number += 1
-                return [self.username, self.follower], 1
+                return {"user_id":self.username, "follower_id":self.follower}, 1
             case 2:
-                return [self.username], 2
+                return {"user_id": self.username}, 2
 
 class Unfollow(Transaction):
     @override
@@ -100,10 +100,10 @@ class Unfollow(Transaction):
         match self.hop_number:
             case 0:
                 self.hop_number += 1
-                return [f"{self.username}-{self.follower}"], 0
+                return {"edge_id":f"{self.username}-{self.follower}"}, 0
             case 1:
                 self.hop_number += 1
-                return [self.username, self.follower], 1
+                return {"user_id":self.username, "follower_id":self.follower}, 1
             case 2:
                 self.hop_number += 1
-                return [self.username], 2
+                return {"user_id": self.username}, 2
--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -3,22 +3,22 @@ from src.transactions import Comment, Post, Follow, Unfollow
 class Test_Transactions():
     def test_Comment(self):
         comment = Comment(1, "Joe", "Hello")
-        assert comment.execute() == ([1, "Joe", "Hello"], 0)
-        assert comment.execute() == (["Joe"], 1)
+        assert comment.execute() == ({"comment_id":1, "user_id":"Joe", "comment":"Hello"}, 0)
+        assert comment.execute() == ({"user_id":"Joe"}, 1)
 
     def test_Post(self):
         post = Post(1, "Joe", "I had a nice day today")
-        assert post.execute() == ([1, "Joe", "I had a nice day today"],0)
-        assert post.execute() == (["Joe"],1)
+        assert post.execute() == ({"post_id":1, "user_id":"Joe", "content":"I had a nice day today"},0)
+        assert post.execute() == ({"user_id": "Joe"},1)
     
     def test_Follow(self):
         follow = Follow("Joe", "Bob")
-        assert follow.execute() == (["Joe-Bob"],0)
-        assert follow.execute() == (["Joe", "Bob"],1)
-        assert follow.execute() == (["Joe"],2)
+        assert follow.execute() == ({"edge_id": "Joe-Bob"},0)
+        assert follow.execute() == ({"user_id":"Joe","follower_id":"Bob"},1)
+        assert follow.execute() == ({"user_id":"Joe"},2)
 
     def test_Unfollow(self):
         unfollow = Unfollow("Joe", "Bob")
-        assert unfollow.execute() == (["Joe-Bob"],0)
-        assert unfollow.execute() == (["Joe", "Bob"],1)
-        assert unfollow.execute() == (["Joe"],2)
+        assert unfollow.execute() == ({"edge_id": "Joe-Bob"},0)
+        assert unfollow.execute() == ({"user_id":"Joe","follower_id":"Bob"},1)
+        assert unfollow.execute() == ({"user_id":"Joe"},2)
