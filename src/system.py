c05a3cbe1dde82f0118d2359007af86cc6811c6d
stevendee37 <stevenwchang01@gmail.com>
2024-12-02 16:03:05 -0800

    Implemented system partitioning and tests.

--- src/system.py
+++ src/system.py
@@ -3,16 +3,14 @@ import os
 import pandas as pd
 
 class DistributedSystem():
-    def __init__(self, sys_name: str):
+    def __init__(self, root_dir, sys_name:str):
         self.sys_name = sys_name
         self.partitions = {}
-        self.root = os.path.join(os.path.join(os.getcwd(), "databases"), self.sys_name)
-        return
+        self.system_dir = os.path.join(os.path.join(root_dir, "systems"), self.sys_name)
     
-    def partitionDatabase(self, file_name, database_class: str):
+    def partitionDatabase(self, db_class:str):
         """ Partitions database based on alphabetical sorting
         Args:
-            file_name: file for unparitioned database
             database_class: database class for one of the predefined values:
                 - profile
                 - graph
@@ -21,22 +19,20 @@ class DistributedSystem():
                 - edges
         """
         # Reads from unpartitioned database (i.e. test-profile.csv, test-graph.csv)
-        if database not in ["profile", "graph", "posts", "comments", "edges"]:
+        if db_class not in ["profile", "graph", "posts", "comments", "edges"]:
             exit("Unsupported Database type")
-        database_path = os.path.join(self.root, f"{file_name}-{database_class}")
-        database = pd.read_csv(database_path)
+        db_path = os.path.join(self.system_dir, f"{self.sys_name}-{db_class}.csv")
+        db = pd.read_csv(db_path)
         
-        output_dir = os.path.join(self.root, "partitions")
+        output_dir = os.path.join(self.system_dir, "partitions")
 
         # Adds data into appropriate partition file based on parition criteria
-        for _, row in database.iterrows():
+        for _, row in db.iterrows():
             username = row['username']
             partition = self.determinePartition(username)
-            partition_file = os.path.join(output_dir, f"{file_name}-{database_class}_{partition}.csv")
+            partition_file = os.path.join(output_dir, f"{self.sys_name}-{db_class}_{partition}.csv")
 
             pd.DataFrame([row]).to_csv(partition_file, mode='a', index=False, header=not os.path.exists(partition_file))
-
-
 
     def determinePartition(self, username: str):
         """ Determines dataset partition to send table entry to.
